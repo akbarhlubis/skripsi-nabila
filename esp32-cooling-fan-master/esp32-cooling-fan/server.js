0469d530fdbc6b381f2bcd47dfe517e98080ef30
akbarhlubis <akbar_hamonangan_lubis@teknokrat.ac.id>
2024-01-05 11:08:47 +0700

    Add .gitignore and package files, and update index.html and server.js

---
+++ esp32-cooling-fan-master/esp32-cooling-fan/server.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        console.log('received: %s', message);
+        // Echo the message back to the client
+        ws.send('Message received: ' + message);
+        // Send a temperature value to the client called tempC
+        ws.send(JSON.stringify({ tempC: Math.floor(Math.random() * 100) }));
+        // send a humidity value to the client called humidity
+        ws.send(JSON.stringify({ humidity: Math.floor(Math.random() * 100) }));
+    });
+
+    ws.send('WebSocket server connected');
+});
